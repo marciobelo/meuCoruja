85825a5d940d5996e56997827966dfc9438f24ca
Eduardo <eduardosantos.ist@gmail.com>
2017-06-30 23:59:47 -0300

    Mudança no título da tela de login

--- fonte/coruja/meu_coruja/index.php
+++ fonte/coruja/meu_coruja/index.php
@@ -5,8 +5,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Boletim</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Meu Coruja</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/login.css">
         
