f43017f0bc0741b0cfc29d85efef9fcfcb4c186f
Eduardo Santos <eduardosantos.ist@gmail.com>
2018-05-06 14:18:41 -0300

    Merge com Coruja

---
+++ fonte/coruja/mmc_gpl/formularios/creditos.php
@@ -0,0 +1,64 @@
+<?php header( "Content-Type: text/html; charset=ISO-8859-1"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <title>Coruja</title>
+
+        <link href="/coruja/estilos/estilo.css" rel="stylesheet" type="text/css" media="all" />
+        <!link href="/coruja/estilos/estilo_siro.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="/coruja/estilos/estilo_menu_horizontal.css" rel="stylesheet" type="text/css" media="screen" />
+        <link href="/coruja/estilos/estilo_form.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="/coruja/estilos/calendar.css" rel="stylesheet" type="text/css" media="screen" />
+        <link href="/coruja/baseCoruja/estilos/estilo_aba_formulario.css" rel="stylesheet" type="text/css" media="screen" />
+        <link rel="shortcut icon" href="/coruja/imagens/favicon.ico" />
+    </head>
+
+    <body>
+        <div id="conteudo">
+            <form name="credito">
+                <fieldset id="fieldsetGeral">
+                    <legend>MMC GPL ( Sistema de Gerência de Perfis e Logs com Módulo de Matriz Curricular ) </legend>
+                    <table cellspacing="2" cellpadding="2">
+                        <tr>
+                            <td>
+                                <img alt="" width="112px" src='/coruja/imagens/carlos.png' title='Carlos Miceli' />
+                            </td>
+                            <td>
+                                <font> <b>Carlos Miceli:</b></font>
+                                Responsável pela idealização gráfica do sistema e 
+                                adaptação de novas tecnologias com o visual atual do sistema como
+                                diagramação em geral (espaçamento, cores, posicionamento dos elementos),
+                                esquematização da busca,
+                                padronização dos ícones e avisos,
+                                graduação por cores das equivalências (sinalização para correlacionar ao estado atual da componente curricular),
+                                elaboração dos testes e documentação.                                
+                                
+                                <br/>
+                                Contato: carloshenrique09@outlook.com
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <img alt="" width="120px" src='/coruja/imagens/dalmo.png' title='Dalmo Lopes' />
+                            </td>
+                            <td>
+                                <font> <b>Dalmo Lopes:</b></font>
+                                Responsavel pelo desenvolvimento das funcionalidades de
+                                manutenção de permissões e grupos de permissões (visualização, criação e edição das permissões e grupos), 
+                                gerência e consulta aos logs, 
+                                criação e validação de matriz curricular proposta (prévia da matriz, criação e edição das componentes curriculares), 
+                                visualização gráfica da matriz curricular proposta (informações de componentes, sinalização de pré-requisitos, carga horária e créditos por perídos), 
+                                impressão de matrizes (curriculares propostas ou curriculares anteriores), 
+                                impressão do fluxograma criado dinamicamente  
+                                <br/>
+                                Contato: lopes.dalmo@gmail.com
+                            </td>
+                        </tr>
+
+                    </table>
+                </fieldset>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ script/migracao/migracao-1.6-para-1.7.sql
@@ -0,0 +1,106 @@
+-- esquema versao
+use Coruja;
+
+ALTER TABLE ComponenteCurricular ADD COLUMN `posicaoPeriodo` INT UNSIGNED DEFAULT 0 NOT NULL;
+
+
+CREATE TABLE IF NOT EXISTS `GrupoFuncao` (
+  `id` INT NOT NULL AUTO_INCREMENT, 
+  `nome` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `FuncaoPorGrupo` (
+  `id` INT NOT NULL AUTO_INCREMENT, 
+  `idGrupo` INT NOT NULL,
+  `idCasoUso` CHAR(16) NOT NULL,
+  PRIMARY KEY (`id`),
+CONSTRAINT `fk_FuncaoPorGrupo_Funcao`
+    FOREIGN KEY (`idCasoUso` )
+    REFERENCES `Funcao` (`idCasoUso` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+CREATE  TABLE IF NOT EXISTS `MatrizCurricularProposta` (
+  `siglaCurso` CHAR(6) NOT NULL ,
+  `idMatrizVigente` INT UNSIGNED NOT NULL ,
+  `totalPeriodos` INT UNSIGNED NOT NULL ,
+  PRIMARY KEY (`siglaCurso`, `idMatrizVigente`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `ComponenteCurricularProposto` (
+`siglaCurso` CHAR(6) NOT NULL ,
+`idMatriz` INT UNSIGNED NOT NULL ,
+`siglaDisciplina` CHAR(6) NOT NULL ,
+`nomeDisciplina` VARCHAR(80) NOT NULL ,
+`creditos` INT UNSIGNED NOT NULL COMMENT 'Quantidade de horas/aula por semana. A disciplina terá uma carga horária no semestre PREVISTA de 20 vezes o número de créditos.' ,
+`cargaHoraria` INT NOT NULL ,
+`periodo` INT UNSIGNED NOT NULL COMMENT 'Período na qual a disciplina se encontra na matriz curricular' ,
+`tipoComponenteCurricular` ENUM('OBRIGATÓRIA','ELETIVA', 'OPTATIVA') NOT NULL ,
+`posicaoPeriodo` INT UNSIGNED NOT NULL ,
+PRIMARY KEY (`siglaDisciplina`, `idMatriz`, `siglaCurso`));
+
+CREATE TABLE IF NOT EXISTS `PreRequisitoProposto` (
+`siglaCurso` CHAR(6) NOT NULL ,
+`idMatriz` INT UNSIGNED NOT NULL ,
+`siglaDisciplina` CHAR(6) NOT NULL ,
+`siglaPreRequisito` CHAR(6) NOT NULL ,
+PRIMARY KEY (`siglaCurso`, `idMatriz`, `siglaDisciplina`, `siglaPreRequisito`));
+
+CREATE TABLE IF NOT EXISTS `EquivalenciaProposta` (
+`siglaCurso` CHAR(6) NOT NULL ,
+`idMatriz` INT UNSIGNED NOT NULL ,
+`siglaDisciplina` CHAR(6) NOT NULL ,
+`siglaEquivalencia` CHAR(6) NOT NULL ,
+PRIMARY KEY (`siglaDisciplina`, `idMatriz`, `siglaCurso`, `siglaEquivalencia`));
+
+
+
+
+-- carga dados basicos
+
+-- MMC-GPL
+-- Novas Permissoes
+INSERT INTO Funcao VALUES ('UC09.01.00', 'Manter Permissões', 'SIM');
+INSERT INTO Funcao VALUES ('UC09.01.01', 'Atribuir Permissão', 'SIM');
+INSERT INTO Funcao VALUES ('UC09.01.02', 'Remover Permissão', 'SIM');
+INSERT INTO Funcao VALUES ('UC09.01.03', 'Atribuir Grupo de Permissões', 'SIM');
+INSERT INTO Funcao VALUES ('UC09.01.04', 'Remover Grupo de Permissões', 'SIM');
+
+-- Permissoes dos grupos
+INSERT INTO Funcao values ('UC09.02.00', 'Manter Grupo de Permissões', 'SIM');
+INSERT INTO Funcao values ('UC09.02.01', 'Incluir Grupo de Permissões', 'SIM');
+INSERT INTO Funcao values ('UC09.02.02', 'Excluir Grupo de Permissões', 'SIM');
+INSERT INTO Funcao values ('UC09.02.03', 'Editar Grupo de Permissões', 'SIM');
+
+-- Gerencia de Log
+INSERT INTO Funcao values ('UC10.01.00', 'Buscar Log', 'SIM');
+
+-- Matriz porposta-- 
+INSERT INTO Funcao values ('UC11.01.00', 'Manter Matriz Curricular Proposta', 'SIM');
+INSERT INTO Funcao values ('UC11.01.01', 'Criar Matriz Curricular Proposta', 'SIM');
+INSERT INTO Funcao values ('UC11.01.02', 'Editar Matriz Curricular Proposta', 'SIM');
+INSERT INTO Funcao values ('UC11.01.02.00', 'Manter Componente Curricular Proposto', 'SIM');
+INSERT INTO Funcao values ('UC11.01.02.01', 'Adicionar Componente Curricular Proposto', 'SIM');
+INSERT INTO Funcao values ('UC11.01.02.02', 'Editar Componente Curricular Proposto', 'SIM');
+INSERT INTO Funcao values ('UC11.01.02.03', 'Excluir Componente Curricular Proposto', 'SIM');
+INSERT INTO Funcao values ('UC11.01.02.04', 'Validar Matriz Curricular Proposta', 'SIM');
+INSERT INTO Funcao values ('UC11.01.03', 'Excluir Matriz Curricular Proposta', 'SIM');
+INSERT INTO Funcao values ('UC11.01.04', 'Imprimir Matriz Curricular', 'SIM');
+
+insert into Permite (idPessoa,idCasoUso)
+ (select 1029,idCasoUso from Funcao f
+	where not exists (select * from Permite pe2 where idPessoa=1029 and pe2.idCasoUso=f.idCasoUso) );
+
+commit;
+
+alter table Login
+ ADD COLUMN `tentativas` INT UNSIGNED DEFAULT 0 NOT NULL;
+
+create table if not exists `LoginErro`
+(
+`dataHoraRegistro` TIMESTAMP NOT NULL DEFAULT NOW(),
+`texto` TEXT not null
+);
+
