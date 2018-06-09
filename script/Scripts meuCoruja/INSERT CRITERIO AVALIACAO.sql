e8a34731729c90393da9e33d02c6f9e8a104925c
ÐiinO Eduardo Santos <eduardosantos.ist@gmail.com>
2018-06-09 14:24:28 -0300

    Botão Sair funcionando offline e Correção no Script do Criterio de Avaliação

--- script/Scripts meuCoruja/INSERT CRITERIO AVALIACAO.sql
+++ script/Scripts meuCoruja/INSERT CRITERIO AVALIACAO.sql
@@ -1,3 +1,3 @@
-INSERT INTO `criterioavaliacao` (`idCriterioAvaliacao`,`rotulo`) VALUES (2,'teste');
-INSERT INTO `itemcriterioavaliacao` (`idItemCriterioAvaliacao`,`idCriterioAvaliacao`,`rotulo`,`descricao`,`ordem`,`tipo`,`formulaCalculo`) VALUES (7,2,'TESTE','ItemCriterioAvaliacaoTeste',7,'LANÇADO',NULL);
-INSERT INTO `turma` (`idTurma`,`siglaCurso`,`idMatriz`,`siglaDisciplina`,`gradeHorario`,`idPeriodoLetivo`,`matriculaProfessor`,`turno`,`tipoSituacaoTurma`,`qtdeTotal`,`dataLiberacaoPautaPeloProfessor`,`idCriterioAvaliacao`) VALUES (38,'TASI',5,'AC2','A',19,'1094200','NOITE','LIBERADA',15,NULL,2);
+INSERT INTO `criterioavaliacao` (`rotulo`) VALUES ('teste');
+INSERT INTO `itemcriterioavaliacao` (`idCriterioAvaliacao`,`rotulo`,`descricao`,`ordem`,`tipo`,`formulaCalculo`) VALUES (2,'TESTE','ItemCriterioAvaliacaoTeste',7,'LANÇADO',NULL);
+update turma set idCriterioavaliacao = 2 where idturma = 38
